9921b7a5c0c6ab5c7f62af356f5979d3494ccf59
LiliaDurnina <166434239+LiliaDurnina@users.noreply.github.com>
2024-09-22 22:56:04 +0300

    h/w

---
+++ 1_week/str2_leetcode.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        palindrome = s[0]
+        maxPalindrome = ""
+        k = -1
+        for i in s:
+            k += 1
+            s2 = s[k:]
+
+            flag = 1
+            while s2.rfind(i) != 0 and flag != 0:
+                if s2[: s2.rfind(i) + 1] == s2[: s2.rfind(i) + 1][::-1]:
+                    palindrome = s2[: s2.rfind(i) + 1]
+                    flag = 0
+                else:
+                    s2 = s2[: s2.rfind(i)]
+            if len(palindrome) > len(maxPalindrome):
+                maxPalindrome = palindrome
+        return maxPalindrome
